8fbc8ffbd231450ac004cab9eb553259a36762ec
mblaul <mattblaul@gmail.com>
2018-05-27 15:57:49 -0400

    Starting to work on frontend, commiting before I mess with Reacy router

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import uuid from "uuid";
 import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -21,7 +20,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         {this.state.todos.map(todo => <li>{todo.title}</li>)}
