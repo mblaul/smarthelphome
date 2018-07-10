491b758138b2115c31cadf079aa08c64003ce462
mblaul <mattblaul@gmail.com>
2018-07-09 21:26:28 -0400

    Removed some unnecessary files

--- client/src/routes/Register.js
+++ client/src/routes/Register.js
@@ -1,41 +1,45 @@
-import React from 'react';
-import 'antd/dist/antd.css';
-import { Input, Button } from 'antd';
+import React from "react";
+import "antd/dist/antd.css";
+import { Input, Button } from "antd";
 
 export default class Register extends React.Component {
+  
+  state = {
+    username: "",
+    password: "",
+    isAdmin: false
+  };
 
-    state = {
-        username: '',
-        password: '',
-        isAdmin: false,
-      }
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-    onChange = (e) => {
-       this.setState({
-           [e.target.name]: e.target.value
-       })
-    }
-      
-    onSubmit = () => {
-        console.log(this.state);
-    }
+  onSubmit = () => {
+    console.log(this.state);
+  };
 
-    render() {
-        return (
-          <div>
-            <Input
-              name='username'
-              placeholder='Username'
-              onChange={e => this.onChange(e)}
-              value={this.state.username} />
-            <Input
-              name='password'
-              placeholder='Password'
-              type='password'
-              onChange={e => this.onChange(e)}
-              value={this.state.password} />
-              <Button onClick={() => this.onSubmit()} type="primary">Submit</Button>
-            </div>  
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Input
+          name="username"
+          placeholder="Username"
+          onChange={e => this.onChange(e)}
+          value={this.state.username}
+        />
+        <Input
+          name="password"
+          placeholder="Password"
+          type="password"
+          onChange={e => this.onChange(e)}
+          value={this.state.password}
+        />
+        <Button onClick={() => this.onSubmit()} type="primary">
+          Submit
+        </Button>
+      </div>
+    );
+  }
 }
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -5,12 +5,25 @@ import Login from "./Login";
 import Register from "./Register";
 import Auth from "./Auth";
 
+function PrivateRoute ({component: Component, authenticated, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authenticated === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+    />
+  )
+}
+
+
 export default () => (
   <Router>
     <Switch>
       <Route exact path="/login" render={props => <Login {...props} />} />
       <Route exact path="/register" render={props => <Register {...props} />} />
       <Route exact path="/auth" render={props => <Auth {...props} />} />
+      <PrivateRoute authenticated={this.state.authenticated} path='/invoices' component={Invoices} />
     </Switch>
   </Router>
 );
